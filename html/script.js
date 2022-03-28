9e067721d678ca2df314ec89c9fcca4c55a5d9bc
ouornek <byouornek@gmail.com>
2022-03-28 08:53:29 +0600

    finished the layout class

---
+++ html/script.js
@@ -0,0 +1,9 @@
+console.log('hey there');
+function showpopup () {
+  var popup = document.getElementById('popup');
+  popup.classList.add('show');
+}
+function hidepopup () {
+  var popup = document.getElementById('popup');
+  popup.classList.remove('show');
+}
